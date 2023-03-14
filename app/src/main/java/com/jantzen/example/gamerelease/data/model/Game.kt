46ab4f6930428673545d1cfc717236db8aa324f1
franziskajantzen <franziska.jantzen12@gmail.com>
2023-03-14 16:17:41 +0100

    init

---
+++ app/src/main/java/com/jantzen/example/gamerelease/data/model/Game.kt
@@ -0,0 +1,11 @@
+package com.jantzen.example.gamerelease.data.model
+
+data class Game (val cover: Int,
+                 val name: String,
+                 val releaseDate: String,
+                 val description: String,
+                 val gameModes: String,
+                 val genres: String,
+                 val plattforms: String,
+                 val categorie: String,
+                 )
---
+++ app/src/main/java/com/jantzen/example/gamerelease/data/test.kt
@@ -0,0 +1,4 @@
+package com.jantzen.example.gamerelease.data
+
+class test {
+}
